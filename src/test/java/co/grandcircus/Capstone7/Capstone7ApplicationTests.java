c15329bfcee609d968f460aeda983c62f61c5ac4
Sam Keim <skeim235@gmail.com>
2020-03-05 10:47:24 -0500

    First commit, starter only

---
+++ src/test/java/co/grandcircus/Capstone7/Capstone7ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.Capstone7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
