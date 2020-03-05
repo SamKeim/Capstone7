f90641bec2590197e09a1877685f8779ff14d033
jillian <jillcenters@protonmail.com>
2020-03-05 13:59:36 -0500

    adding api classes

---
+++ src/main/java/co/grandcircus/Capstone7/entities/Hits.java
@@ -0,0 +1,23 @@
+package co.grandcircus.Capstone7.entities;
+
+public class Hits {
+	private Recipe recipe;
+
+	
+	
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return "Hits [recipe=" + recipe + "]";
+	}
+	
+}
