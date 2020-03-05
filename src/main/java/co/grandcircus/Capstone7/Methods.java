5a9005e5b3dcd48b0b0365feab984295f9803abe
Sam Keim <skeim235@gmail.com>
2020-03-05 13:08:10 -0500

    Continued work on controllers

---
+++ src/main/java/co/grandcircus/Capstone7/Methods.java
@@ -0,0 +1,11 @@
+package co.grandcircus.Capstone7;
+
+import java.util.List;
+
+public class Methods {
+	
+	public List<Recipe> findFavorites(){
+		return null;
+		//FIXME
+	}
+}
--- src/main/java/co/grandcircus/Capstone7/RecipeController.java
+++ src/main/java/co/grandcircus/Capstone7/RecipeController.java
@@ -1,6 +1,54 @@
 package co.grandcircus.Capstone7;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @Controller
 public class RecipeController {
-
+	
+	@Autowired
+	RecipeDao rDao;
+	
+	@RequestMapping("/")
+	public ModelAndView showHome() {
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping("/search")
+	public ModelAndView showSearch() {
+		return new ModelAndView("search");
+	}
+	
+	@PostMapping("/search")
+	public ModelAndView showResults(
+			@RequestParam(required=false) String label,
+			@RequestParam(required=false) String dietLabels,
+			@RequestParam(required=false) String healthLabels,
+			@RequestParam(required=false) Integer from,
+			@RequestParam(required=false) Integer to,
+			RedirectAttributes redir
+			) {
+		try {
+		List<Recipe> recipeList = rDao.findByCriteria(label, dietLabels, healthLabels, from, to);
+		return new ModelAndView("result", "list", recipeList);
+		} catch (RestClientException e) {
+			redir.addFlashAttribute("message", "No results found!");
+			return new ModelAndView("/search");
+		}
+	}
+	
+	@RequestMapping("/fav")
+	public ModelAndView showFavorites() {
+		List<Recipe> favList = getFavorites();
+		
+	}
+	
 }
