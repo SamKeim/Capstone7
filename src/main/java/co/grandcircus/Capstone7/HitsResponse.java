4abd98e5d0d391aad453d72a18353f4b93acd14e
jillian <jillcenters@protonmail.com>
2020-03-05 15:12:30 -0500

    api class update

---
+++ src/main/java/co/grandcircus/Capstone7/HitsResponse.java
@@ -0,0 +1,14 @@
+package co.grandcircus.Capstone7;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import co.grandcircus.Capstone7.entities.Hits;
+import co.grandcircus.Capstone7.entities.Recipe;
+
+public class HitsResponse {
+	@JsonProperty("hits")
+	private List<Recipe> recipes;
+	
+}
--- src/main/java/co/grandcircus/Capstone7/Methods.java
+++ src/main/java/co/grandcircus/Capstone7/Methods.java
@@ -2,6 +2,8 @@ package co.grandcircus.Capstone7;
 
 import java.util.List;
 
+import co.grandcircus.Capstone7.entities.Recipe;
+
 public class Methods {
 	
 	public List<Recipe> findFavorites(){
